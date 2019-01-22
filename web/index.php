a7a1f85251ddbc40e531b9305a216bc492821a5c
Mozgus <retailnet@rambler.ru>
2019-01-22 14:51:15 +0300

    Init Yii2 framework minimal

--- web/index.php
+++ web/index.php
@@ -1,3 +1,14 @@
 <?php
 
-echo '<h1>Video School</h1>';
+use yii\base\InvalidConfigException;
+use yii\web\Application;
+
+define('YII_DEBUG', true);
+require(__DIR__ . '/../vendor/autoload.php');
+require(__DIR__ . '/../vendor/yiisoft/yii2/Yii.php');
+$config = require(__DIR__ . '/../config/web.php');
+try {
+    $app = new Application($config);
+} catch (InvalidConfigException $e) {
+}
+$app->run();
