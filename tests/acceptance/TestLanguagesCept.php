02a550959150b34fd850b37db482ad69c58d41b2
Mozgus <retailnet@rambler.ru>
2019-01-23 21:46:43 +0300

    Some prepares for localization

--- tests/acceptance/TestLanguagesCept.php
+++ tests/acceptance/TestLanguagesCept.php
@@ -7,8 +7,10 @@ $I->see('Viseo School');
 
 $I->amOnPage('/en');
 $I->see('Welcome!');
+$I->dontSee('Добро пожаловать!');
 $I->see('Русский', 'a');
 
 $I->amOnPage('/ru');
 $I->see('Добро пожаловать!');
+$I->dontSee('Welcome!');
 $I->see('English', 'a');
