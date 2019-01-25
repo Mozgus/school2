67bbfe30bc50187eb73b9f690153bd8c698754ee
Mozgus <retailnet@rambler.ru>
2019-01-25 23:48:33 +0300

    Prepare for internationalization, pretty url and debug module

--- config/web.php
+++ config/web.php
@@ -2,5 +2,21 @@
 
 return [
     'id' => 'school-web',
-    'basePath' => realpath(__DIR__ . '/../')
+    'basePath' => realpath(__DIR__ . '/../'),
+    'language' => 'ru',
+    'bootstrap' => [
+        'debug'
+    ],
+    'components' => [
+        'urlManager' => [
+            'enablePrettyUrl' => true,
+            'showScriptName' => false
+        ],
+        'request'=>[
+            'cookieValidationKey'=>'m1kd4a7c25xs8n4b7cx3n4df0'
+        ]
+    ],
+    'modules' => [
+        'debug' => 'yii\debug\Module'
+    ]
 ];
--- views/layouts/main.php
+++ views/layouts/main.php
@@ -1,11 +1,19 @@
+<?php $this->beginPage() ?>
 <!doctype html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <title>Document</title>
+    <?php $this->head() ?>
 </head>
 <body>
+<?php $this->beginBody() ?>
 <h1>Video School</h1>
-<?= $content ?>
+<div class="container" style="margin-top: 80px">
+    <?= $content ?>
+
+</div>
+<?php $this->endBody() ?>
 </body>
 </html>
+<?php $this->endPage() ?>
